b0364f5a54b3b201397417f6100f733a03c33d8b
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-05-31 19:36:11 +0300

    Initial commit of HRMS Extention project

---
+++ src/main/java/mg/hrms/HrmsExtension.java
@@ -0,0 +1,23 @@
+package mg.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class HrmsExtension {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmsExtension.class, args);
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
---
+++ src/test/java/mg/hrms/HrmsExtensionTests.java
@@ -0,0 +1,13 @@
+package mg.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsExtensionTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
