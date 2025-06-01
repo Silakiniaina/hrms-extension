0e0f8630d5bebd5879a5405a819a17800f270156
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-01 12:12:49 +0300

    config : Add JacksonConfig and RestTemplateConfig.

---
+++ src/main/java/mg/hrms/config/RestTemplateConfig.java
@@ -0,0 +1,24 @@
+package mg.hrms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory clientHttpRequestFactory) {
+        return new RestTemplate(clientHttpRequestFactory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+        return factory;
+    }
+}
