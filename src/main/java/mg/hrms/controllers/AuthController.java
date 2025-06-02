0a4c62bc6c9f7ae2c99d1a318c1621789a9ccd13
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-02 12:42:13 +0300

    feat : Create endpoint : POST /auth.

--- src/main/java/mg/hrms/controllers/AuthController.java
+++ src/main/java/mg/hrms/controllers/AuthController.java
@@ -1,12 +1,28 @@
 package mg.hrms.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+import mg.hrms.models.User;
+import mg.hrms.services.AuthService;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    /* -------------------------------------------------------------------------- */
+    /*                                 Constructor                                */
+    /* -------------------------------------------------------------------------- */
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
     
     /* -------------------------------------------------------------------------- */
     /*      Display the login page when the user accesses the /auth endpoint      */
@@ -14,5 +30,29 @@ public class AuthController {
     @GetMapping
     public String showLoginPage() {
         return "pages/login";
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*         Handle login requests when the user submits the login form         */
+    /* -------------------------------------------------------------------------- */
+    @PostMapping
+    public String handleLogin(@RequestParam String username, @RequestParam String password, HttpSession session,
+            Model model) {
+        try {
+            if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+                model.addAttribute("error", "Username and password are required");
+                return "pages/login";
+            }
+
+            User user = authService.login(username, password);
+            session.setAttribute("sid", user.getSid());
+            session.setAttribute("user", user);
+            model.addAttribute("success", "User : "+user.getFullName() +" successfully connected");
+
+            return "pages/login";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "pages/login";
+        }
     }
 }
