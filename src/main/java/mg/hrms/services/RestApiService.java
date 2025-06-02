6d70516cdd81f79413e54981e06cfabed763a7ff
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-02 22:45:13 +0300

    feat : Create function executeApiCall.

--- src/main/java/mg/hrms/services/RestApiService.java
+++ src/main/java/mg/hrms/services/RestApiService.java
@@ -1,8 +1,21 @@
 package mg.hrms.services;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import mg.hrms.models.User;
+import mg.hrms.utils.ApiUtils;
 
 @Service
 public class RestApiService {
@@ -24,5 +37,49 @@ public class RestApiService {
     /* -------------------------------------------------------------------------- */
     public RestApiService(RestTemplate r){
         this.restTemplate = r;
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                          API Call for ErpNEXT API                          */
+    /* -------------------------------------------------------------------------- */
+    @SuppressWarnings("deprecation")
+    public <T, R> ResponseEntity<R> executeApiCall(String url, HttpMethod method, T requestBody,
+            User user, ParameterizedTypeReference<R> responseType) throws Exception {
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            ApiUtils.setUserCookie(user, headers);
+
+            HttpEntity<T> entity = new HttpEntity<>(requestBody, headers);
+
+            ResponseEntity<R> response = restTemplate.exchange(
+                    url,
+                    method,
+                    entity,
+                    responseType);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                return response;
+            }
+
+            throw new Exception("HTTP " + response.getStatusCodeValue());
+
+        } catch (HttpClientErrorException e) {
+            String errorMessage;
+            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                errorMessage = "Unauthorized access to API: Invalid credentials or session";
+            } else if (e.getStatusCode() == HttpStatus.FORBIDDEN) {
+                errorMessage = "Permission denied: User lacks access to resource " + url + ". Response: "
+                        + e.getResponseBodyAsString();
+            } else {
+                errorMessage = "API request failed: HTTP " + e.getStatusCode() + ". Response: "
+                        + e.getResponseBodyAsString();
+            }
+            throw new Exception(errorMessage, e);
+        } catch (Exception e) {
+            throw new Exception("API request failed: " + e.getMessage(), e);
+        }
     }
 }
