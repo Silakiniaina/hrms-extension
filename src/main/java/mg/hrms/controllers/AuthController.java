6cfc61f43385704b11e7aa61d4078ae73fb281d1
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-02 15:18:02 +0300

    feat : Update success auth redirection to home.

--- src/main/java/mg/hrms/controllers/AuthController.java
+++ src/main/java/mg/hrms/controllers/AuthController.java
@@ -47,9 +47,9 @@ public class AuthController {
             User user = authService.login(username, password);
             session.setAttribute("sid", user.getSid());
             session.setAttribute("user", user);
-            model.addAttribute("success", "User : "+user.getFullName() +" successfully connected");
-
-            return "pages/login";
+            String success =  "User : "+user.getFullName() +" successfully connected";
+            
+            return "redirect:/home?success="+success;
         } catch (Exception e) {
             model.addAttribute("error", e.getMessage());
             return "pages/login";
