7d31b405625004c775d796251f44715d9b80bd98
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-03 09:11:22 +0300

    feat : Create function to buildUrl for ErpNext API Call.

--- src/main/java/mg/hrms/utils/ApiUtils.java
+++ src/main/java/mg/hrms/utils/ApiUtils.java
@@ -1,11 +1,15 @@
 package mg.hrms.utils;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import mg.hrms.models.User;
 
 public class ApiUtils {
 
+    /* -------------------------------------------------------------------------- */
+    /*                     Set the user cookien on the headers                    */
+    /* -------------------------------------------------------------------------- */
     public static void setUserCookie(User user, HttpHeaders headers) {
         if (user == null) {
             throw new IllegalArgumentException("User cannot be null");
@@ -19,5 +23,27 @@ public class ApiUtils {
         headers.add("Cookie", "full_name=" + (user.getFullName() != null ? user.getFullName() : ""));
         headers.add("Cookie", "user_lang=" + (user.getUserLang() != null ? user.getUserLang() : ""));
         headers.add("Cookie", "system_user=" + (user.getSystemUser() != null ? user.getSystemUser() : ""));
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                       Build url for ErpNext API call                       */
+    /* -------------------------------------------------------------------------- */
+    @SuppressWarnings("deprecation")
+    public static String buildUrl(String baseUrl, String[] fields) {
+        // Convert the fields array to a JSON array string
+        StringBuilder fieldsJson = new StringBuilder("[");
+        for (int i = 0; i < fields.length; i++) {
+            fieldsJson.append("\"").append(fields[i]).append("\"");
+            if (i < fields.length - 1) {
+                fieldsJson.append(",");
+            }
+        }
+        fieldsJson.append("]");
+
+        return UriComponentsBuilder
+                .fromHttpUrl(baseUrl)
+                .queryParam("fields", fieldsJson.toString())
+                .build()
+                .toUriString();
     }
 }
