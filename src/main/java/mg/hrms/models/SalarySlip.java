c412dc2c6fbd2fce61c6facdd4bd988ea5338963
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-04 12:13:49 +0300

    feat : Create model Salary Slip.

---
+++ src/main/java/mg/hrms/models/SalarySlip.java
@@ -0,0 +1,39 @@
+package mg.hrms.models;
+
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SalarySlip {
+    @JsonProperty("name")
+    private String slipId;
+
+    @JsonProperty("employee")
+    private String employeeId;
+
+    @JsonProperty("employee_name")
+    private String employeeName;
+
+    @JsonProperty("posting_date")
+    private Date postingDate;
+
+    @JsonProperty("gross_pay")
+    private Double grossPay;
+
+    @JsonProperty("net_pay")
+    private Double netPay;
+
+    @JsonProperty("status")
+    private String status;
+}
