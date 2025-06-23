aab960c54fe1c7e952322ca14bdebbc48ced7e9f
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-23 23:42:52 +0300

    feat : Create Operation utils.

---
+++ src/main/java/mg/hrms/utils/OperationUtils.java
@@ -0,0 +1,10 @@
+package mg.hrms.utils;
+
+import org.slf4j.Logger;
+
+public class OperationUtils {
+    
+    public static void logStep(String message, Logger logger) {
+        logger.info(message);
+    }
+}
