61a9ebb4faec9c9d001cf4a58aa931a51d6ec926
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-23 23:46:38 +0300

    refactor : Migrate searchFrappeDocumets function in FrappeService.

--- src/main/java/mg/hrms/services/FrappeService.java
+++ src/main/java/mg/hrms/services/FrappeService.java
@@ -1,5 +1,7 @@
 package mg.hrms.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -21,7 +23,10 @@ public class FrappeService {
         this.restApiService = restApiService; 
     }
 
-    @SuppressWarnings({ "null", "unchecked", "static-access" })
+    /* -------------------------------------------------------------------------- */
+    /*                        Find a document with its name                       */
+    /* -------------------------------------------------------------------------- */
+    @SuppressWarnings({ "null", "unchecked" })
     public Map<String, Object> getFrappeDocument(String doctype, String name, User user) {
         try {
             String url = restApiService.buildResourceUrl(doctype, name, null);
@@ -40,8 +45,41 @@ public class FrappeService {
             }
             return null;
         } catch (Exception e) {
-            OperationUtils.logStep("Failed to get document " + doctype + "/" + name + ": " + e.getMessage(), this.logger);
+            OperationUtils.logStep("Failed to get document " + doctype + "/" + name + ": " + e.getMessage(), logger);
             return null;
+        }
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                       Search for object with filters                       */
+    /* -------------------------------------------------------------------------- */
+    @SuppressWarnings({ "unchecked", "null" })
+    public List<Map<String, Object>> searchFrappeDocuments(String doctype, Map<String, Object> filters, User user) {
+        try {
+            // Convertir les filtres Map en List<String[]> pour buildUrl
+            List<String[]> filtersList = new ArrayList<>();
+            for (Map.Entry<String, Object> entry : filters.entrySet()) {
+                filtersList.add(new String[] { entry.getKey(), "=", entry.getValue().toString() });
+            }
+
+            String url = restApiService.buildUrl(doctype, null, filtersList);
+            ResponseEntity<Map<String, Object>> response = restApiService.executeApiCall(
+                    url,
+                    HttpMethod.GET,
+                    null,
+                    user,
+                    new ParameterizedTypeReference<Map<String, Object>>() {
+                    });
+
+            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
+
+                List<Map<String, Object>> data = (List<Map<String, Object>>) response.getBody().get("data");
+                return data;
+            }
+            return new ArrayList<>();
+        } catch (Exception e) {
+            OperationUtils.logStep("Failed to search documents " + doctype + ": " + e.getMessage(), logger);
+            return new ArrayList<>();
         }
     }
 }
