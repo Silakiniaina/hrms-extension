24873167a1bfa4de4aad3c9c6c49f9033755ba5f
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-23 23:57:10 +0300

    refactor : Migrate formatDate to OperationUtils.

--- src/main/java/mg/hrms/utils/OperationUtils.java
+++ src/main/java/mg/hrms/utils/OperationUtils.java
@@ -1,10 +1,36 @@
 package mg.hrms.utils;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+
 import org.slf4j.Logger;
+
+import mg.hrms.exception.DateFormatException;
 
 public class OperationUtils {
     
+    /* -------------------------------------------------------------------------- */
+    /*                             Log operation steps                            */
+    /* -------------------------------------------------------------------------- */
     public static void logStep(String message, Logger logger) {
         logger.info(message);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                          Format date to mm/DD/yyyy                         */
+    /* -------------------------------------------------------------------------- */
+    public static String formatDate(String date) throws DateFormatException {
+        if (date == null)
+            return null;
+
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.FRANCE);
+            LocalDate localDate = LocalDate.parse(date, formatter);
+            return localDate.format(formatter); 
+        } catch (DateTimeParseException e) {
+            throw new DateFormatException("Format date not valid, format should be : dd/MM/yyyy");
+        }
     }
 }
