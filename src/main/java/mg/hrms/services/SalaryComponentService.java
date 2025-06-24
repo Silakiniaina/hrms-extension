e4af09fbdefeb057cd2d870bd5928523ca1532e5
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-24 10:43:49 +0300

    refactor : Update SalaryComponentService to use frappeService functions.

--- src/main/java/mg/hrms/services/SalaryComponentService.java
+++ src/main/java/mg/hrms/services/SalaryComponentService.java
@@ -1,18 +1,9 @@
 package mg.hrms.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import mg.hrms.models.SalaryComponent;
-import java.util.List;
 
 @Service
 public class SalaryComponentService {
-    private final Logger logger = LoggerFactory.getLogger(SalaryComponentService.class);
+    
 
-    public List<SalaryComponent> getComponentsForStructure(String structureId) {
-        // Implémentez la logique de récupération depuis l'API
-        // ou une base de données locale si vous cachez ces données
-        return List.of();
-    }
 }
