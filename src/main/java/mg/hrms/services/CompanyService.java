7e1e4af8151f826ca330711684d88eedb6855006
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-06-24 10:42:35 +0300

    refactor : Update CompanyService to use frappeService functions.

--- src/main/java/mg/hrms/services/CompanyService.java
+++ src/main/java/mg/hrms/services/CompanyService.java
@@ -3,10 +3,10 @@ package mg.hrms.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import mg.hrms.models.Company;
 import mg.hrms.models.User;
+import mg.hrms.utils.OperationUtils;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,36 +16,32 @@ import java.util.Map;
 public class CompanyService {
 
     private static final Logger logger = LoggerFactory.getLogger(CompanyService.class);
-    private final RestApiService restApiService;
+    private final FrappeService frappeService;
     private final ObjectMapper objectMapper;
 
-    public CompanyService(RestApiService restApiService, ObjectMapper objectMapper) {
-        this.restApiService = restApiService;
+    public CompanyService(ObjectMapper objectMapper, FrappeService frappeService) {
         this.objectMapper = objectMapper;
+        this.frappeService = frappeService;
     }
 
     /* -------------------------------------------------------------------------- */
     /*                         Method to get All companies                        */
     /* -------------------------------------------------------------------------- */
-    @SuppressWarnings("null")
     public List<Company> getAll(User user) throws Exception {
-        logger.info("Fetching all companies for user: {}", user.getFullName());
-        String[] fields = {"name"};
-        String apiUrl = restApiService.buildUrl("Company", fields, null);
+        OperationUtils.logStep("Fetching all companies", logger);
+        String[] fields = new String[]{"name"};
+        List<Map<String, Object>> response = frappeService.searchFrappeDocuments("Company",fields, null, user);
 
-        var response = restApiService.executeApiCall(
-                apiUrl, HttpMethod.GET, null, user, new ParameterizedTypeReference<Map<String, Object>>() {});
-
-        if (response.getBody() == null || response.getBody().get("data") == null) {
+        if (response == null) {
             logger.error("Companies data not found for user: {}", user.getFullName());
             throw new Exception("Companies data not found");
         }
 
         List<Company> companies = objectMapper.convertValue(
-                response.getBody().get("data"),
+                response,
                 objectMapper.getTypeFactory().constructCollectionType(List.class, Company.class)
         );
-        logger.info("Retrieved {} companies for user: {}", companies.size(), user.getFullName());
+        OperationUtils.logStep("Retrieve "+companies.size()+" compnies", logger);
         return companies;
     }
 }
